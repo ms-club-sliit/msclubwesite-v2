9135da88bfccdd00a802df68fcb56550a715b74b
senuravihanjayadeva <senurajayadeva@gmail.com>
2021-11-14 18:35:06 +0530

    implemeted contact us page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,9 +1,6 @@
-import Home from './home';
-import Event from './event';
-import About from './about';
+import Home from "./home";
+import Event from "./event";
+import About from "./about";
+import Contact from "./contact";
 
-export {
-  Home,
-  Event,
-  About
-};
+export { Home, Event, About, Contact };
--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { NavBar, Footer } from '../components';
-import {Home,Event, About} from '../pages';
+import {Home,Event, About, Contact} from '../pages';
 
 const AppRoutes: React.FC = () => (
   <div>
     <Router>
       <NavBar />
       {/* Route Declaration - Start */}
-      <About/>
+      <Contact/>
       {/* Route Declaration - End */}
     </Router>
     <Footer />
