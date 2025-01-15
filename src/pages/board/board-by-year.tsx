b7ab4ca7cd5d8c990766b1229c2a6fb9d4a060a4
Tharindu Dananjaya Abenayaka <155418943+dananjayaabeytd@users.noreply.github.com>
2025-01-15 22:55:58 +0530

    Replace year navigators with a dropdown

--- src/pages/board/board-by-year.tsx
+++ src/pages/board/board-by-year.tsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { IBoard, IBoardMemberDetails } from "../../interfaces/BoardInterface";
-import boardData from "../../data/BoardSectionData.json";
-import BoardMemberCard from "./board-member-card";
-import ts from "../../locales/en-US/translation.json";
+import React, { useState, useEffect } from 'react';
+import { IBoard, IBoardMemberDetails } from '../../interfaces/BoardInterface';
+import boardData from '../../data/BoardSectionData.json';
+import BoardMemberCard from './board-member-card';
+import ts from '../../locales/en-US/translation.json';
 
 let translation = ts.translation;
 
 const BoardByYear: React.FC = () => {
   const [id, setid] = useState<number>(0);
-  const [year, setyear] = useState<string>("");
+  const [year, setyear] = useState<string>('');
   const [boardList, setboardList] = useState<IBoard[]>([]);
   const [boardMemberList, setboardMemberList] = useState<IBoardMemberDetails[]>(
     []
@@ -26,10 +26,14 @@ const BoardByYear: React.FC = () => {
     return num + 1;
   };
 
-  const handleClick = (id: number, year: string) => {
-    setid(id - 1);
-    setyear(year);
-    setboardMemberList(boardList[id - 1].board);
+  const handleDropdownChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const selectedId = parseInt(event.target.value, 10);
+    const selectedBoard = boardList[selectedId - 1];
+    setid(selectedId - 1);
+    setyear(selectedBoard.year);
+    setboardMemberList(selectedBoard.board);
   };
 
   const nextBoard = {
@@ -37,37 +41,34 @@ const BoardByYear: React.FC = () => {
     year,
   };
   return (
-    <div className="board-year">
-      <div className="hero-section-bg" />
-      <div className="row">
-        <span className="title">
-          {translation.label["board-board-by-year-title"]} {nextBoard.year} -{" "}
+    <div className='board-year'>
+      <div className='hero-section-bg' />
+      <div className='row'>
+        <span className='title'>
+          {translation.label['board-board-by-year-title']} {nextBoard.year} -{' '}
           {incrementNumber(nextBoard.year)}
-          <span className="wave-hand-emoji" />
+          <span className='wave-hand-emoji' />
         </span>
-        <div className="row">
-          {boardList.map((board) => (
-            <div
-              className="col-lg-2 col-md-3 col-sm-6 board-col"
-              key={board.id}
-            >
-              <button
-                type="button"
-                className="button btn text-center board-button"
-                onClick={() => handleClick(board.id, board.year)}
-              >
-                <span className="calendar-emoji"></span>
-                {board.year}/{incrementNumber(board.year.slice(-2))}
-              </button>
-              &nbsp;&nbsp;&nbsp;
-            </div>
-          ))}
+
+        {/* Dropdown */}
+        <div className='col-md-12 board-dropdown-wrapper'>
+          <select
+            className='board-dropdown button btn text-center board-button'
+            onChange={handleDropdownChange}
+          >
+            {boardList.map(board => (
+              <option key={board.id} value={board.id}>
+                {board.year} / {incrementNumber(board.year.slice(-2))}
+              </option>
+            ))}
+          </select>
         </div>
-        <div className="col-md-12">
-          <div className="row">
-            {boardMemberList.map((member) => (
+
+        <div className='col-md-12'>
+          <div className='row'>
+            {boardMemberList.map(member => (
               <div
-                className="col d-flex justify-content-center"
+                className='col d-flex justify-content-center'
                 key={member.id}
               >
                 <BoardMemberCard
